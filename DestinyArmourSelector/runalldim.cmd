e6289bf3b0f5a6d18a9a1853c4af42d30dddb408
Gudge <GudgePersonal@users.noreply.github.com>
2018-12-22 01:44:27 -0800

    Change input file path in runalldim.cmd
    Change input file path to match location DIM stores in by default.

--- DestinyArmourSelector/runalldim.cmd
+++ DestinyArmourSelector/runalldim.cmd
@@ -1,5 +1,5 @@
-.\bin\Debug\DestinyArmourSelector -inputFile D:\git\d2armourselector\data\destinyArmor.csv -type "Helmet" > helmets.log
-.\bin\Debug\DestinyArmourSelector -inputFile D:\git\d2armourselector\data\destinyArmor.csv -type "Gloves" > gloves.log
-.\bin\Debug\DestinyArmourSelector -inputFile D:\git\d2armourselector\data\destinyArmor.csv -type "Chest" > chests.log
-.\bin\Debug\DestinyArmourSelector -inputFile D:\git\d2armourselector\data\destinyArmor.csv -type "Legs" > legs.log
-.\bin\Debug\DestinyArmourSelector -inputFile D:\git\d2armourselector\data\destinyArmor.csv -type "ClassItem" > classitems.log
+.\bin\Debug\DestinyArmourSelector -inputFile C:\Users\Martin\Downloads\destinyArmor.csv -type "Helmet" > helmets.log
+.\bin\Debug\DestinyArmourSelector -inputFile C:\Users\Martin\Downloads\destinyArmor.csv -type "Gloves" > gloves.log
+.\bin\Debug\DestinyArmourSelector -inputFile C:\Users\Martin\Downloads\destinyArmor.csv -type "Chest" > chests.log
+.\bin\Debug\DestinyArmourSelector -inputFile C:\Users\Martin\Downloads\destinyArmor.csv -type "Legs" > legs.log
+.\bin\Debug\DestinyArmourSelector -inputFile C:\Users\Martin\Downloads\destinyArmor.csv -type "ClassItem" > classitems.log
