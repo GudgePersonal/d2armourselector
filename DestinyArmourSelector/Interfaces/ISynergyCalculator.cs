46aca4818aad3f8226f1494d88c062930fba2140
Gudge <45739002+GudgePersonal@users.noreply.github.com>
2018-12-22 01:26:58 -0800

    Refactor synergy calculation (#6)
    Move synergy calculation to 'SynergyCalculator classes, modeled using
    ISynergyCalculator.
    
    Add a SimpleSynergyCalculator class and move synergy related fields and
    methods from DIMArmourPieceFactory to this new class.
    
    Add a StaticSynergyCalculator class that simple returns a string passed
    to the constructor.
    
    Amend ArmourPiece to take an ISynergyCalculator and use that to implement
    the getter for the Synergy property.
    
    Remove the setter for the Synergy property.
    
    Amend ArmourPieceFactory to create a StaticSynergyCalculator for each
    ArmourPiece.
    
    Amend DIMArmourPieceFactory to create a SimpleSynergyCalculator for each
    ArmourPiece.
    
    Add an Interfaces folder and namespace to hold both the new ISynergyCalculator
    interface and the existing IArmourPieceFactory interface.

---
+++ DestinyArmourSelector/Interfaces/ISynergyCalculator.cs
@@ -0,0 +1,9 @@
+﻿// Copyright Small & Fast 2018
+
+namespace DestinyArmourSelector.Interfaces
+{
+    public interface ISynergyCalculator
+    {
+        string CalculateSynergy(PerkGroup primaryPerks, PerkGroup secondaryPerks);
+    }
+}
---
+++ DestinyArmourSelector/StaticSynergyCalculator.cs
@@ -0,0 +1,21 @@
+﻿// Copyright Small & Fast 2018
+
+namespace DestinyArmourSelector
+{
+    using Interfaces;
+
+    public class StaticSynergyCalculator : ISynergyCalculator
+    {
+        private readonly string _synergy = string.Empty;
+
+        public StaticSynergyCalculator(string synergy)
+        {
+            _synergy = synergy;
+        }
+
+        public string CalculateSynergy(PerkGroup primaryPerks, PerkGroup secondaryPerks)
+        {
+            return _synergy;
+        }
+    }
+}
